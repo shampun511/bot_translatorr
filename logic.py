cebe0bcd0ced1469580c53cf844b7bd06f4671ac
shampun511 <den-belov00@mail.ru>
2024-11-30 18:31:45 +0500

    https://github.com/shampun511/bot_translatorr.git

---
+++ logic.py
@@ -0,0 +1,43 @@
+# Задание №3
+import requests
+from collections import defaultdict
+from translate import Translator
+
+
+
+# Задание №5
+qwestions = {'как тебя зовут' : "Я супер-крутой-бот и мое ппредназначение помогать тебе!",
+             "сколько тебе лет" : "Это слишком философский вопрос"}
+
+
+class TextAnalysis():   
+    
+    # Задание №1
+    memory = defaultdict(list)
+    def __init__(self, text, owner):
+
+        # Задание №2
+        TextAnalysis.memory[owner].append(self)
+        self.text = text
+        self.translation = self.__translate(self.text, "ru", "en")
+
+        # Задание №6
+        if self.text.lower() in qwestions.keys():
+            pass
+        else:
+            self.response = self.get_answer() 
+    
+    def get_answer(self):
+        res = self.__translate("I don't know how to help", "en", "ru")
+        return res
+
+    def __translate(self, text, from_lang, to_lang):
+        try:
+            # Задание №4
+            translator = Translator(from_lang=from_lang, to_lang=to_lang)
+            translation = translator.translate(text)
+            return translation
+        except:
+            return "Перевод не удался"
+
+
